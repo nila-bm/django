22cbe744dbd8c19bd3dcab5f3a6263f0a6f70747
nila <nilabmd2@gmail.com>
2026-07-18 17:48:10 +0330

    working with for loop and dispalynig lists in dtl

--- core/dashboard/views.py
+++ core/dashboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+lst=["nila","reza","nika"]
 
 def welcome_page(request):
-    return render(request,'dashboard/index.html',{"name":"Nila"})
+    return render(request,'dashboard/index.html',{"names":lst})
