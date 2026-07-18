c22024b777a0515897578e0116a8f601e57ac99b
nila <nilabmd2@gmail.com>
2026-07-18 22:05:42 +0330

    url tag

--- core/dashboard/views.py
+++ core/dashboard/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 dictionary={"google":"https://google.com",
             "apple":"https://apple.com",
             "yahoo":"https://yahoo.com",}
+def index(request):
+    return HttpResponse('root page')
 
 def welcome_page(request):
     return render(request,'dashboard/index.html',{"links":dictionary})
