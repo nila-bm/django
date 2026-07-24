fbdd0a5d3d3d9bb7da85c727cd2c2a8b4a58f57c
nila <nilabmd2@gmail.com>
2026-07-24 13:48:08 +0330

    show and return database in browser

--- core/farm/views.py
+++ core/farm/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Farm
 
 # Create your views here.
+
+def farm(request):
+    farms=Farm.objects.all()
+    return render(request,'farm/index.html',{'farms':farms})
